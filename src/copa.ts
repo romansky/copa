847aded3145a7d52ad53e1f3b06cf66386f7f483
romanskym1 <roman.land@gmail.com>
2026-02-09 11:12:41 +0200

    release: 1.7.6

--- src/copa.ts
+++ src/copa.ts
@@ -54,7 +54,7 @@ async function handleToCommand(file: string, options: { errors?: boolean, tokens
 program
     .name('copa')
     .description('CoPa: Prompt Engineering Templating Language and CLI Tool ')
-    .version('1.7.5');
+    .version('1.7.6');
 
 program
     .command('to <file>')
